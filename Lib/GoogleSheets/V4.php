eafe0eb68ada8406493fda3ff10f6702d804295d
Jan Ptacek <jan.ptacek@gmail.com>
2017-03-13 09:45:33 +0100

    import prices from google documents

---
+++ Lib/GoogleSheets/V4.php
@@ -0,0 +1,110 @@
+<?php
+// define APP constant for cli invocation
+defined('APP') or define('APP', dirname(dirname(__DIR__)).'/');
+
+require_once APP . '/Vendor/autoload.php';
+
+define('APPLICATION_NAME',  'Google Sheets API PHP Quickstart');
+define('CREDENTIALS_PATH',   APP . 'Config/google_sheets/sheets.googleapis.com-php-credentials.json');
+define('CLIENT_SECRET_PATH', APP . 'Config/google_sheets/client_secret.json');
+// If modifying these scopes, delete your previously saved credentials
+// at CREDENTIALS_PATH
+define('SCOPES', implode(' ', array(
+  Google_Service_Sheets::SPREADSHEETS_READONLY)
+));
+
+class V4 {
+    /**
+     * Returns an authorized API client.
+     * @return Google_Client the authorized client object
+     */
+    public function getClient() {
+      $client = new Google_Client();
+      $client->setApplicationName(APPLICATION_NAME);
+      $client->setScopes(SCOPES);
+      $client->setAuthConfig(CLIENT_SECRET_PATH);
+      $client->setAccessType('offline');
+
+      // Load previously authorized credentials from a file.
+      if (file_exists(CREDENTIALS_PATH)) {
+        $accessToken = json_decode(file_get_contents(CREDENTIALS_PATH), true);
+      } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
+
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+        // Store the credentials to disk.
+        if(!file_exists(dirname(CREDENTIALS_PATH))) {
+          mkdir(dirname(CREDENTIALS_PATH), 0700, true);
+        }
+        file_put_contents(CREDENTIALS_PATH, json_encode($accessToken));
+        printf("Credentials saved to %s\n", CREDENTIALS_PATH);
+      }
+      $client->setAccessToken($accessToken);
+
+      // Refresh the token if it's expired.
+      if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents(CREDENTIALS_PATH, json_encode($client->getAccessToken()));
+      }
+      return $client;
+    }
+
+    // Get the API client and construct the service object.
+    public function getData($spreadsheetId, $range) {
+        $client = $this->getClient();
+        $service = new Google_Service_Sheets($client);
+
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+        return $values;
+
+        if (count($values) == 0) {
+          print "No data found.\n";
+        } else {
+          print "member, student, early, czk, eur:\n";
+          foreach ($values as $row) {
+            // Print columns A and E, which correspond to indices 0 and 4.
+            printf("%s, %s, %s, %s, %s\n", $row[0], $row[1], $row[2], $row[3], $row[4]);
+          }
+        }
+    }
+
+    public function getPrices() {
+        $spreadsheetId = '1D2OpAdVu01xhcbVqQv3N9YVg4FpSAPbIw5_igi-JsBQ';
+        $range = 'Prices!A2:E'; # skip first header row
+
+        $values = $this->getData($spreadsheetId, $range);
+        $prices = [];
+        foreach ($values as $row) {
+            $czk = array_pop($row);
+            $eur = array_pop($row);
+            sort($row);
+            $key = implode('-', $row);
+            $prices[$key] = compact('czk', 'eur');
+        }
+
+        $prices = json_encode($prices, JSON_PRETTY_PRINT);
+        return $prices;
+    }
+
+    public function savePrices($filename) {
+        $prices = $this->getPrices();
+        if (file_put_contents($filename, $prices) === false) {
+            return "Error writting prices.\n";
+        } else {
+            return "$filename written ok\n";
+        }
+    }
+}
+
+// called from command line to authenticate and store the data
+if (php_sapi_name() == 'cli') {
+    $v4 = new V4();
+    echo $v4->savePrices(APP . 'Config/prices.json');
+}
--- README.md
+++ README.md
@@ -6,7 +6,12 @@ Registrations + Booking
 $ npm install
 $ composer install
 $ bower install
+$ php Lib/GoogleSheets/V4.php   # download prices data from google sheets
 $ grunt
+
+# CREATE USER 'eamt'@'localhost' IDENTIFIED BY 'password';
+# GRANT ALL PRIVILEGES ON eamt.* TO 'eamt'@'localhost';
+# FLUSH PRIVILEGES;
 
 $ vim Config/database.php # edit db credentials
 $ mysql -u user -p eamt < schema.sql
